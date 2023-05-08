3d97dde617a7ed3db003e3133e047bf04019929e
KGian9 <truongkgiang012@gmail.com>
2023-05-09 00:07:26 +0700

    Build Base

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import React from "react";
-import { Route, Routes, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import News from "./pages/News";
-import About from "./pages/About";
 import "./App.scss";
 
 function App() {
